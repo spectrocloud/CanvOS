8cc9ed5bcbd21a5ea005956152e956e815919aed
Justin Barksdale <justin@spectrocloud.com>
2023-05-10 15:09:56 +0000

    updated output

--- earthly.sh
+++ earthly.sh
@@ -4,8 +4,9 @@ source .arg
 docker run --privileged -v /var/run/docker.sock:/var/run/docker.sock --rm --env EARTHLY_BUILD_ARGS -t -v "$(pwd)":/workspace -v earthly-tmp:/tmp/earthly:rw earthly/earthly:v0.7.4 --allow-privileged "$@"
 
 echo -e '###################################################################################################'
-echo -e '\n\nPASTE THE CONTENTS BELOW INTO YOUR CLUSTER PROFILE IN PALETTE BELOW THE "OPTIONS" ATTRIBUTE\n\n'
+echo -e '\nPASTE THE CONTENTS BELOW INTO YOUR CLUSTER PROFILE IN PALETTE BELOW THE "OPTIONS" ATTRIBUTE\n'
 echo -e '###################################################################################################'
+echo -e '\n'
 echo -e '  system.uri: "{{ .spectro.pack.edge-native-byoi.options.system.registry }}/{{ .spectro.pack.edge-native-byoi.options.system.repo }}:{{ .spectro.pack.edge-native-byoi.options.system.k8sDistribution }}-{{ .spectro.system.kubernetes.version }}-{{ .spectro.pack.edge-native-byoi.options.system.peVersion }}-{{ .spectro.pack.edge-native-byoi.options.system.customTag }}"'
 echo -e '\n'
 echo -e "  system.registry: $IMAGE_REGISTRY"
