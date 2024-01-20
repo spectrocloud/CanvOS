2f2af7843c6f6b040b521a2a314b6bb186a48613
Boobalan <boobalan@spectrocloud.com>
2024-01-19 16:27:05 -0800

    SLEM build changed

--- slem/README.md
+++ slem/README.md
@@ -1,6 +1,9 @@
-# slem
+# SUSE Linux Enterprise Micro
 
-slem base image needs to built on the slem server.
-A registration code is need to build the slem base image.
+## Pre-requisites :
+* A host with `Docker`, SLES Micro installed
+* Registration code to register with SUSE
+* If you wish to override the BASE_IMAGE, make sure to use a container image that has zypper installed in it 
 
+## Steps to build the image:
 ./build.sh <REGISTRATION_CODE>
--- slem/build.sh
+++ slem/build.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-set -ex
+if [[ -z "$1" ]]; then
+  echo "ERROR : Registration code is empty !"
+  echo "Re-run this utility with SUSE Registration code in the args."
+  echo "Example : ./build.sh 123456789"
+  exit 1
+fi
 
+set -ex
 REGISTRATION_CODE=$1
-mkdir /var/slem
+mkdir -p /var/slem
+yes | cp ./Dockerfile /var/slem
 cd /var/slem
-mkdir repos
-mkdir services
+mkdir -p repos
+mkdir -p services
 cd repos/
-mkdir SUSE
-mkdir opensuse
+mkdir -p SUSE
+mkdir -p opensuse
 cd SUSE
 cp /etc/zypp/repos.d/SUSE*.repo .
 cd ../../services/
@@ -22,7 +29,8 @@ autorefresh=0
 baseurl=http://download.opensuse.org/distribution/leap/15.5/repo/oss/
 EOF
 cd ../..
-SUSEConnect -r $REGISTRATION_CODE
+#SUSEConnect -r $REGISTRATION_CODE
+transactional-update register -r $REGISTRATION_CODE
 systemctl restart docker
 transactional-update -n pkg install docker
 transactional-update -n register -p PackageHub/15.4/x86_64
