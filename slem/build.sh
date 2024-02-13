938a4875dc158254ec7a17dbf3f7492e2e2b8859
Boobalan Rathinam <57693963+brathina-spectro@users.noreply.github.com>
2024-02-13 12:30:01 -0800

    Updated SLEM build script for Kairos v2.4.3 (#126)
    * SLEM build changed
    
    * Fix kairos framework version to 2.4.3
    
    * Fix the docker tag name
    
    * slem kairos 2.4.3 changes

--- slem/build.sh
+++ slem/build.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
+if [[ -z "$1" ]]; then
+  echo "ERROR : Registration code is empty !"
+  echo "Re-run this utility with SUSE Registration code in the args."
+  echo "Example : ./build.sh 123456789"
+  exit 1
+fi
+REGISTRATION_CODE=$1
+
 set -ex
 
-REGISTRATION_CODE=$1
-mkdir /var/slem
+mkdir -p /var/slem
+yes | cp ./Dockerfile /var/slem
 cd /var/slem
-mkdir repos
-mkdir services
+mkdir -p repos
+mkdir -p services
 cd repos/
-mkdir SUSE
-mkdir opensuse
+mkdir -p SUSE
+mkdir -p opensuse
 cd SUSE
 cp /etc/zypp/repos.d/SUSE*.repo .
 cd ../../services/
 cp /etc/zypp/services.d/*.service .
 cd ../repos/opensuse/
+
 cat > opensuse-oss.repo <<EOF
 [opensuse-oss]
 enabled=1
@@ -22,9 +31,9 @@ autorefresh=0
 baseurl=http://download.opensuse.org/distribution/leap/15.5/repo/oss/
 EOF
 cd ../..
-SUSEConnect -r $REGISTRATION_CODE
-systemctl restart docker
-transactional-update -n pkg install docker
-transactional-update -n register -p PackageHub/15.4/x86_64
-docker build -t slem-base-image:v243 .
 
+#SUSEConnect -r $REGISTRATION_CODE
+transactional-update register -r $REGISTRATION_CODE
+transactional-update -n pkg install docker
+
+docker build -t slem-base:kairos-v2.4.3 .
