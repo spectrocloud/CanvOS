dd01dc5989020ec25ab984912e0184123ff3a546
Santhosh <santhosh@spectrocloud.com>
2025-03-15 08:12:43 -0700

    update base image as inputs for fips images

--- rhel-fips/build.sh
+++ rhel-fips/build.sh
@@ -7,11 +7,3 @@ BASE_IMAGE="${3:-rhel-byoi-fips}"
 
 # Build the container image
 docker build --build-arg USERNAME="$USERNAME" --build-arg PASSWORD="$PASSWORD" -t "$BASE_IMAGE" .
-
-docker run -v "$PWD"/build:/tmp/auroraboot \
-        -v /var/run/docker.sock:/var/run/docker.sock \
-        --rm quay.io/kairos/auroraboot \
-        --set container_image=docker://"$BASE_IMAGE" \
-        --set "disable_http_server=true" \
-        --set "disable_netboot=true" \
-        --set "state_dir=/tmp/auroraboot"
