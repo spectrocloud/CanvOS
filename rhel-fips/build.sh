9fedd348ee8c27715c0148abee98ef79f47b3e17
Kiran Kilingar Nadumane <kirankn8@gmail.com>
2025-08-21 17:59:59 -0700

    Refactor Earthfile and build scripts for improved clarity and consistency
    - Cleaned up whitespace and formatting in the Earthfile to enhance readability.
    - Removed unnecessary `--no-cache` option from Docker build commands in RHEL and Ubuntu FIPS build scripts for efficiency.
    - Ensured consistent formatting across various sections of the Earthfile to maintain a uniform style.

--- rhel-fips/build.sh
+++ rhel-fips/build.sh
@@ -6,4 +6,4 @@ PASSWORD=$2
 BASE_IMAGE="${3:-rhel-byoi-fips}"
 
 # Build the container image
-docker build --no-cache --progress=plain --build-arg USERNAME="$USERNAME" --build-arg PASSWORD="$PASSWORD" -t "$BASE_IMAGE" .
+docker build --build-arg USERNAME="$USERNAME" --build-arg PASSWORD="$PASSWORD" -t "$BASE_IMAGE" .
