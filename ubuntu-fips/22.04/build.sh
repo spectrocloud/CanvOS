e38735099574c9844901c99ac0ee7e1113bf6ddf
Kiran Kilingar Nadumane <kirankn8@gmail.com>
2025-09-11 14:32:15 -0700

    Update build script and add new Dockerfile for Ubuntu 22.04 FIPS with enhanced configuration and package management

--- ubuntu-fips/22.04/build.sh
+++ ubuntu-fips/22.04/build.sh
@@ -1,3 +1,3 @@
 BASE_IMAGE="${1:-ubuntu-jammy-fips}"
 VERSION=22.04
-DOCKER_BUILDKIT=1 docker build --secret id=pro-attach-config,src=pro-attach-config.yaml -t "$BASE_IMAGE" -f "Dockerfile.ubuntu$VERSION-fips" .
+DOCKER_BUILDKIT=1 docker build --secret id=pro-attach-config,src=pro-attach-config.yaml -t "$BASE_IMAGE" -f "Dockerfile.ubuntu$VERSION-fips-new" .
